8a86ba7328a2185e3d3fc4f0496a0045c4c8df97
Connor <connor.mcintosh@hotmail.com>
2022-05-10 14:05:08 +1000

    Progress on sign up page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
-import './App.css';
+import "./App.css";
 
-import { db } from './firebase.js';
-import { collection , onSnapshot } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import TinderCards from './TinderCards';
-import Header from './Header'
-
-
+import { db } from "./firebase.js";
+import { collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import TinderCards from "./TinderCards";
+import Header from "./Header";
+import SignUp from "./SignUp";
+import { BrowserRouter as Router,Route,Link,Routes } from "react-router-dom";
+import ChatPage from "./ChatPage";
 
 function App() {
+  const [animals, setAnimals] = useState([]);
 
-  const [animals, setAnimals] = useState([])
-
-  useEffect(()=>{
-
-    onSnapshot(collection(db,'Animals'),(snapshot)=>{
-
-      setAnimals(snapshot.docs.map(doc => doc.data()))
-
-  })},[])
+  useEffect(() => {
+    onSnapshot(collection(db, "Animals"), (snapshot) => {
+      setAnimals(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
 
   return (
-
     <div className="App">
-      <Header/>
-
-      <TinderCards animal={animals} />
-
-     
-
+      <Header />
+      <Router>
+        <Routes>
+          <Route path ="/signup" element ={<SignUp/>}></Route>
+          <Route path="/chat" element={<ChatPage />}></Route>
+          <Route path="/" element={<TinderCards animal={animals} />}></Route>
+        </Routes>
+      </Router>
     </div>
-
   );
-
 }
-
-
 
 export default App;
